60e58046954eb819d4c7c9a3fb184121dab7bf43
Cesar Wahyu Cahya A <cesar.wahyu10@gmail.com>
2016-10-25 09:47:54 +0700

    first commit tugas project2

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl4102030/nyam_nyamyuk/MainActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.xiirpl4102030.nyam_nyamyuk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
