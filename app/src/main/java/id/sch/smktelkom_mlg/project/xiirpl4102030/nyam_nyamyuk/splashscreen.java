8eff70a4c885b0ec6a51ab8045cd2aff5c0434bf
Cesar Wahyu Cahya A <cesar.wahyu10@gmail.com>
2016-11-20 14:58:17 +0700

    Second Splashscreen commit.

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl4102030/nyam_nyamyuk/splashscreen.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl4102030/nyam_nyamyuk/splashscreen.java
@@ -1,5 +1,38 @@
 package id.sch.smktelkom_mlg.project.xiirpl4102030.nyam_nyamyuk;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class splashscreen {
+public class splashscreen extends Activity {
+    private static int splashInterval = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(splashscreen.this, MainActivity.class);
+
+                startActivity(i);
+
+                this.finish();
+            }
+
+            private void finish() {
+
+            }
+        }, splashInterval);
+    }
 }
