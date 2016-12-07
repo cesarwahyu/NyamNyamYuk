641f9e7a7ae1627d8845c5771ea4b82244630db8
Cesar Wahyu Cahya A <cesar.wahyu10@gmail.com>
2016-12-07 21:14:20 +0700

    RecyclerView List Card.

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl4102030/nyam_nyamyuk/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl4102030/nyam_nyamyuk/MainActivity.java
@@ -2,9 +2,14 @@ package id.sch.smktelkom_mlg.project.xiirpl4102030.nyam_nyamyuk;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -17,20 +22,33 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.ArrayList;
+
+import adapter.RestoranAdapter;
+import model.Restoran;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    ArrayList<Restoran> mList = new ArrayList<>();
+    RestoranAdapter mAdapter;
     private Button buttonDaftar;
     private EditText editTextEmail;
     private EditText editTextPassword;
     private EditText editTextNama;
     private TextView textViewMasuk;
-
     private ProgressDialog progressDialog;
     private FirebaseAuth firebaseAuth;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        mAdapter = new RestoranAdapter(mList);
+        recyclerView.setAdapter(mAdapter);
+
+        fillData();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -51,6 +69,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         buttonDaftar.setOnClickListener(this);
         textViewMasuk.setOnClickListener(this);
+    }
+
+    private void fillData() {
+        Resources resources = getResources();
+        String[] arJudul = resources.getStringArray(R.array.places);
+        String[] arDeskripsi = resources.getStringArray(R.array.place_desc);
+        TypedArray a = resources.obtainTypedArray(R.array.places_picture);
+        Drawable[] arFoto = new Drawable[a.length()];
+
+        for (int i = 0; i < arFoto.length; i++) {
+            arFoto[i] = a.getDrawable(i);
+        }
+        a.recycle();
+
+        for (int i = 0; i < arJudul.length; i++) {
+            mList.add(new Restoran(arJudul[i], arDeskripsi[i], arFoto[i]));
+        }
+        mAdapter.notifyDataSetChanged();
     }
 
     private void DaftarUser() {
